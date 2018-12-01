621bde55213635c08199eb2d79126f22741b4067
elitegoliath <elitegoliath13@gmail.com>
2018-12-01 12:11:30 -0600

    Started setting up spawn controllers and APC prefab

---
+++ LD43 Yank Train/Assets/Scripts/Enemy Transports/APCController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APCController : MonoBehaviour {
+
+    private TransportSpawnController spawnController = new TransportSpawnController();
+
+    private Transform spawnPoint;
+    private Transform destination;
+
+    // Use this for initialization
+    void Start () {
+        //spawnPoint = spawnController.GenerateSpawnPoint();
+        //destination = spawnController.GenerateWaypoint();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LD43 Yank Train/Assets/Scripts/Enemy Transports/TransportSpawnController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportSpawnController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void GenerateSpawnPoint()
+    {
+        // TODO: Find a random point to spawn outside of the arena.
+    }
+
+    public void GenerateWaypoint()
+    {
+        // TODO: Find a random location inside the arena to go to.
+    }
+}
