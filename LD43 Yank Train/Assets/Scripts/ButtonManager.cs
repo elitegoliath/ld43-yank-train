b45021317024fe552ee5e19e085e7d8495227c3d
elitegoliath <elitegoliath13@gmail.com>
2018-11-30 21:52:45 -0600

    Got main menu buttons working

---
+++ LD43 Yank Train/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+    private const string arenaSceneName = "GameArena";
+    private const string mainMenuSceneName = "GameArena";
+
+    public void loadArenaScene()
+    {
+        SceneManager.LoadScene(arenaSceneName);
+    }
+
+    public void loadMainMenuScene()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
