5a10ef79579ef26490faaf9af5bd12a66ee52ae8
elitegoliath <elitegoliath13@gmail.com>
2018-12-04 17:41:10 -0600

    things seem to be working. i dunno

--- LD43 Yank Train/Assets/Scripts/Mechanics/SelfDestructExplosion.cs
+++ LD43 Yank Train/Assets/Scripts/Mechanics/SelfDestructExplosion.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class SelfDestructExplosion : MonoBehaviour {
     public GameObject explosionParticles;
     public AudioClip explosionSound;
-
-	// Use this for initialization
-	void Start () {
+    
+	private void Start () {
         GameObject explosion = Instantiate(explosionParticles);
         explosion.transform.position = transform.position;
         explosion.transform.localScale = new Vector3(2, 2, 2);
         AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+    }
 
-        Destroy(gameObject, 0.1f);
+    private void LateUpdate()
+    {
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
